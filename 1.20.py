a56236580a0e6186f22776b3e8eb327c92eba45f
duyle138 <77941092+duyle138@users.noreply.github.com>
2021-09-15 20:22:17 -0500

    Add files via upload

---
+++ 1.20.py
@@ -0,0 +1,8 @@
+#Duy Le 1913048
+user_num = int(input('Enter integer:\n'))
+print('You entered:', user_num)
+print(user_num, 'squared is', user_num * user_num)
+print('And', user_num, 'cubed is', user_num * user_num * user_num, '!!')
+user_num1 = int(input('Enter another integer:\n'))
+print(user_num, '+', user_num1, 'is', user_num + user_num1)
+print(user_num, '*', user_num1, 'is', user_num * user_num1)

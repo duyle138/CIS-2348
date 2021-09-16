608b299d211e7170a9e52d134936a20a0d3480b1
duyle138 <77941092+duyle138@users.noreply.github.com>
2021-09-15 21:36:42 -0500

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,12 @@
+# Duy Le 1913048
+print('Birthday Calculator')
+print('Current day')
+month = int(input('Month:\n'))
+day = int(input('Day:\n'))
+year = int(input('Year:\n'))
+print('Birthday')
+month1 = int(input('Month:\n'))
+day1 = int(input('Day:\n'))
+year1 = int(input('Year:\n'))
+if month == month1 and day == day1 and year == year1:
+    print("Happy Birthday!")
